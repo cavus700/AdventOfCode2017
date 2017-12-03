16df786dbd59386a54b03a7a9c9f45c5fc4363fb
robin.hubbig <robin.hubbig@udo.edu>
2017-12-03 19:00:05 +0100

    Solved Day 3.1

--- AdventOfCode/Source/Day3.cpp
+++ AdventOfCode/Source/Day3.cpp
@@ -13,7 +13,28 @@ Day3::~Day3()
 
 void Day3::startA(vector<string> input)
 {
-	
+	int number = stoi(input[0]);
+	int ring = 1;
+	int elements = 9;
+	int elementsOnRing = 8;
+
+	while (elements < number) {
+		ring++;
+		elementsOnRing += 8;
+		elements += elementsOnRing;
+	}
+
+	int indexOnRing = elementsOnRing - (elements - number);
+
+	int quarterOfRing = elementsOnRing / 4;
+
+	int indexInQuarter = indexOnRing % quarterOfRing;
+
+	int distanceToMiddleOfQuarter = (indexInQuarter)-(quarterOfRing) / 2;
+	if (distanceToMiddleOfQuarter < 0)
+		distanceToMiddleOfQuarter *= -1;
+
+	cout << "Steps to port: " << distanceToMiddleOfQuarter + ring << endl;
 }
 
 void Day3::startB(vector<string> input)
