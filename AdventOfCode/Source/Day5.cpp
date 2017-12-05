3d712fb7c2e713a66b466740d20a828160780dbe
Robin.Hubbig <robin.hubbig@udo.edu>
2017-12-05 08:33:11 +0100

    Solved Day 5

--- AdventOfCode/Source/Day5.cpp
+++ AdventOfCode/Source/Day5.cpp
@@ -13,10 +13,47 @@ Day5::~Day5()
 
 void Day5::startA(vector<string> input)
 {
+    vector<int> maze;
 
+    for (string s : input) {
+        maze.push_back(stoi(s));
+    }
+
+    int pos = 0;
+    int steps = 0;
+    int offset = 0;
+
+    while (pos >= 0 && pos < maze.size()) {
+        offset = maze[pos];
+        maze[pos]++;
+        pos += offset;
+        steps++;
+    }
+
+    cout << "Result: " << steps << endl;
 }
 
 void Day5::startB(vector<string> input)
 {
+    vector<int> maze;
 
+    for (string s : input) {
+        maze.push_back(stoi(s));
+    }
+
+    int pos = 0;
+    int steps = 0;
+    int offset = 0;
+
+    while (pos >= 0 && pos < maze.size()) {
+        offset = maze[pos];
+        if (offset >= 3)
+            maze[pos]--;
+        else
+            maze[pos]++;
+        pos += offset;
+        steps++;
+    }
+
+    cout << "Result: " << steps << endl;
 }
