d500de4edba6fda3805f2270a3fef04ecc2e2c90
Robin Hubbig <robin.hubbig@swyx.com>
2017-12-06 08:43:43 +0100

    Solved Day6.1

--- AdventOfCode/Header/Day6.h
+++ AdventOfCode/Header/Day6.h
@@ -11,3 +11,42 @@ public:
 	void startB(vector<string> input);
 };
 
+class MemoryManager {
+public:
+    MemoryManager(vector<int> banks);
+
+    void cycle();
+    string getHash() const {
+        string s = "";
+        for (int i : m_banks)
+            s += to_string(i) + ",";
+        return s;
+    }
+    int getCycles() { return m_runs; }
+
+    bool operator==(const MemoryManager &other) const
+    {
+        return (this->getHash() == other.getHash());
+    }
+    
+
+private:
+    vector<int> m_banks;
+    int m_runs;
+};
+
+
+namespace std {
+    template <>
+    struct std::hash<MemoryManager>
+    {
+        std::size_t operator()(const MemoryManager& s) const
+        {
+            using std::size_t;
+            using std::hash;
+            using std::string;
+            return hash<string>()(s.getHash());
+        }
+    };
+}
+
--- AdventOfCode/Source/day6.cpp
+++ AdventOfCode/Source/day6.cpp
@@ -13,10 +13,55 @@ Day6::~Day6()
 
 void Day6::startA(vector<string> input)
 {
-	
+    istringstream istr(input[0]);
+    istream_iterator<int> iter(istr);
+    istream_iterator<int> eof;
+    vector<int> banks(iter, eof);
+    MemoryManager mgr(banks);
+    unordered_map<MemoryManager, string> map;
+
+    map[mgr] = mgr.getHash();
+
+    while (true) {
+        mgr.cycle();
+
+        if (map[mgr] == mgr.getHash()) {
+            break;
+        }
+        else {
+            map[mgr] = mgr.getHash();
+        }
+    }
+
+    cout << "Result: " << mgr.getCycles() << endl;
 }
 
 void Day6::startB(vector<string> input)
 {
 	
+}
+
+MemoryManager::MemoryManager(vector<int> banks)
+{
+    m_runs = 0;
+    m_banks = banks;
+}
+
+void MemoryManager::cycle()
+{
+    auto maxIter = max_element(m_banks.begin(), m_banks.end());
+    int num = *maxIter;
+    *maxIter = 0;
+
+    while (num > 0) {
+        maxIter++;
+
+        if (maxIter == m_banks.end())
+            maxIter = m_banks.begin();
+        
+        *maxIter = *maxIter + 1;
+        num--;
+    }
+
+    m_runs++;
 }
