02378c78dba518f91f3eaa57f53852450f2b1995
Robin.Hubbig <robin.hubbig@udo.edu>
2017-12-13 22:44:39 +0100

    Solved Day13

--- AdventOfCode/Source/Day12.cpp
+++ AdventOfCode/Source/Day12.cpp
@@ -13,12 +13,81 @@ Day12::~Day12()
 
 }
 
-void Day12::startA(vector<string> a_input)
-{
-	
+void mark(vector<string>& program, vector<vector<string>>& programs, unordered_map<string, bool>& connections, int& count) {
+    if (connections[program[0]]) {
+        return;
+    }
+    connections[program[0]] = true;
+    count++;
+    for (int i = 2; i < program.size(); i++) {
+        mark(programs[stoi(program[i])], programs, connections, count);
+    }
+    return;
 }
 
-void Day12::startB(vector<string> a_input)
-{
+void mark(vector<string>& program, vector<vector<string>>& programs, unordered_map<string, bool>& connections) {
+    if (connections[program[0]]) {
+        return;
+    }
+    connections[program[0]] = true;
+    for (int i = 2; i < program.size(); i++) {
+        mark(programs[stoi(program[i])], programs, connections);
+    }
+    return;
+}
 
+void Day12::startA(vector<string> input)
+{
+    int programsConnected = 0;
+
+    unordered_map<string, bool> connections;
+    vector<vector<string>> programs;
+
+    for (auto line : input) {
+        istringstream istr(line);
+        istream_iterator<string> iter(istr);
+        istream_iterator<string> eof;
+        vector<string> program(iter, eof);
+        
+        for (int i = 2; i < program.size() - 1; i++)
+            program[i] = program[i].substr(0, program[i].size() - 1);
+
+        connections[program[0]] = false;
+
+        programs.push_back(program);
+    }
+    
+    mark(programs[0],programs, connections, programsConnected);
+
+    cout << "Result: " << programsConnected << endl;
+}
+
+void Day12::startB(vector<string> input)
+{
+    int groups = 0;
+
+    unordered_map<string, bool> connections;
+    vector<vector<string>> programs;
+
+    for (auto line : input) {
+        istringstream istr(line);
+        istream_iterator<string> iter(istr);
+        istream_iterator<string> eof;
+        vector<string> program(iter, eof);
+
+        for (int i = 2; i < program.size() - 1; i++)
+            program[i] = program[i].substr(0, program[i].size() - 1);
+
+        connections[program[0]] = false;
+
+        programs.push_back(program);
+    }
+
+    for (auto prog : programs) {
+        if (!connections[prog[0]])
+            groups++;
+        mark(prog, programs, connections);
+    }
+
+    cout << "Result: " << groups << endl;
 }
